e4d252a85b2db6e295c9eeb0e198d8a79c2faf76
JormmungandM <misha.bond1432@gmail.com>
2022-12-13 07:19:53 +0200

    Initial commit

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+
+    [SerializeField]
+    private int HealPoint;
+    [SerializeField]
+    private int AttackDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void MoveHero(){
+        
+    }
+
+
+}
